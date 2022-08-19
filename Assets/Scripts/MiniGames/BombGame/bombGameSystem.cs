38b5c9e99c1dd64f99211b3cad3eed4808b4253b
Pomecs <pedro.silva@academiadecodigo.org>
2022-08-19 15:32:38 +0100

    Bomb moves

--- Assets/Scripts/MiniGames/BombGame/bombGameSystem.cs
+++ Assets/Scripts/MiniGames/BombGame/bombGameSystem.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class bombGameSystem : MonoBehaviour
 {
     public int MAX_POSITION = 47;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject bomb;
+    private RectTransform rectTransform;
+
+    void Awake(){
+        rectTransform = bomb.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float randomSpeed = Random.Range(2f, 6f);
+        float randomSpeed = Random.Range(5f, 15f);
+        Vector2 position = rectTransform.anchoredPosition;
 
-        if(transform.position.x >= MAX_POSITION){
-            transform.position = new Vector3(-MAX_POSITION, 0, 0);
+        if(position.x >= MAX_POSITION){
+            position = new Vector2(-MAX_POSITION, 0);
+            Debug.Log($"new position: {rectTransform.anchoredPosition}");
         }
 
-        transform.Translate(randomSpeed * Time.deltaTime, 0, 0);
+        Debug.Log($"position: {rectTransform.anchoredPosition}");
+        position.x += randomSpeed * Time.deltaTime;
+
+        rectTransform.anchoredPosition = position;
     }
 }
